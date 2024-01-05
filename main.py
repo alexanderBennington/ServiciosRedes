b95b11294d83ec4b3458044678e100080cfaf8de
Sitio <ksanchezt1500@alumno.ipn.mx>
2024-01-04 23:35:38 -0600

    flask e index

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from flask import Flask, render_template
 
-def print_hi(name):
-    print(f'Hi, {name}')
+app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    print_hi('Alexander')
